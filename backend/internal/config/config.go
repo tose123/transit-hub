35939a210ba7127704531ff96b649f6fd02e1a84
nobody <nobody@noreply.com>
2026-07-15 18:50:23 +0800

    chore: prepare v26.7.151847 release

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,7 +11,7 @@ const (
 	defaultPort       = "10621"
 	defaultRedisURL   = "redis://127.0.0.1:6379/0"
 	defaultPublicDir  = "/app/public"
-	defaultAppVersion = "v26.7.151832"
+	defaultAppVersion = "v26.7.151847"
 )
 
 type Config struct {
